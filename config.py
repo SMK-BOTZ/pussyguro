1da626f9110061cf24c01891741a17f2e47312c5
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2024-07-30 13:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,68 +1,67 @@
-# (©)CodeXBotz
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Bot token from @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-# Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
 
-# Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7259590228:AAEKoANUwZUuvd8FUVyOQ3Cbxe_49H32vlo")
 
-# Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "16575077"))
 
-# OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "1c8c0bcb55c14e0fd8078058966b6a11")
 
-# Port
-PORT = os.environ.get("PORT", "")
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002213043042"))
 
-# Database
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "1702061654"))
 
-# Force sub channel id, if you want to enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+#Port
+PORT = os.environ.get("PORT", "8080")
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", ""))
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://madarazbotz:O8WtNAEReh6ohJEt@cluster0.9mosuuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "pomoibot")
 
-# Start message
-START_MSG = os.environ.get("START_MESSAGE", "")
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001757250028"))
 
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "<b> Hᴇʟʟᴏ {first}🙌\n\nI ᴀᴍ ᴀ Fɪʟᴇ [ᴍᴏᴠɪᴇ/ꜱᴇʀɪᴇꜱ/ᴀɴɪᴍᴇ] Pʀᴏᴠɪᴅᴇʀ ʙᴏᴛ \nSᴜʙꜱᴄʀɪʙᴇ ᴛᴏ ᴍʏ Cʜᴀɴɴᴇʟ ᴛᴏ ɢᴇᴛ Fɪʟᴇꜱ ☠️\n\nMᴏᴠɪᴇ/Sᴇʀɪᴇꜱ ~ <a href='https://t.me/+g2ccWFbI2XJkMTE9'>Jᴏɪɴ Hᴇʀᴇ</a>\n\nPᴏᴡᴇʀᴇᴅ ʙʏ <a href='https://t.me/BinaryQuest'>Bɪɴᴀʀʏ Qᴜᴇꜱᴛ 🖤</a></b>")
 try:
-    ADMINS = []
+    ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
-    raise Exception("Your Admins list does not contain valid integers.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
-# Force sub message
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join : <a href='https://t.me/+g2ccWFbI2XJkMTE9'>Cʟɪᴄᴋ Mᴇ 🖤</a></b>")
 
-# Set your Custom Caption here, Keep None for Disable Custom Caption
+#set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-# Set True if you want to prevent users from forwarding files from bot
+#set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-# Set true if you want to disable your Channel Posts Share button
+#Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
     DISABLE_CHANNEL_BUTTON = True
 else:
     DISABLE_CHANNEL_BUTTON = False
 
-BOT_STATS_TEXT = os.environ.get("BOT_STATS_TEXT", "")
-USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "")
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
 
-LOG_FILE_NAME = os.environ.get("LOG_FILE_NAME", "")
+LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -78,6 +77,7 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
