0f47de61db319cf96bb83afd34dfaa6468930a7c
Binary-Quest <163310128+Binary-Quest@users.noreply.github.com>
2024-07-31 12:40:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b>Tʜᴀɴᴋ Yᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ Oᴜʀ ʙᴏᴛ..!\n\nPʟᴇᴀꜱᴇ Jᴏɪɴ - <a href='https://t.me/+g2ccWFbI2XJkMTE9'>Lᴀᴛᴇꜱᴛ Mᴏᴠɪᴇꜱ 🔥</a>\n\nTᴏ ɢᴇᴛ Aʟʟ ʟᴀᴛᴇꜱᴛ Mᴏᴠɪᴇ/Sᴇʀɪᴇꜱ 🖤</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
